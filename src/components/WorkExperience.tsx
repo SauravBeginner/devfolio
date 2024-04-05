0081912518e445b81d0eaad87bde42787ce05c1f
SauravBeginner <kundusaurav1996@gmail.com>
2024-04-06 02:15:17 +0530

    resume fixed

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -131,7 +131,7 @@ const dummyContent = [
         <li>Built internal dashboards using MERN Stack technology.</li>
       </ul>
     ),
-    badge: "Rupeek Fintech Fintech Pvt Ltd",
+    badge: "Rupeek Fintech Pvt Ltd",
     image:
       "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     date: "02/2021 - 05/2022",
