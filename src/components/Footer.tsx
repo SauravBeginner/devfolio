006c4206df256f165001fff6b2ded6d2bbf791eb
SauravBeginner <kundusaurav1996@gmail.com>
2024-04-05 22:51:09 +0530

    footer fixed for sm screen

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,51 +5,15 @@ function Footer() {
   return (
     <section className="relative overflow-hidden py-8">
       <div className="container relative z-10 mx-auto px-4">
-        <div className="-m-8 flex flex-wrap items-center justify-between">
-          <div className="w-auto p-8">
+        <div className="-my-8 flex flex-wrap items-center justify-center md:justify-between">
+          <div className="w-auto px-4 md:px-8 py-0 md:py-8 ">
             <a href="#">
               <div className="inline-flex items-center">
-                <span className="ml-4 text-lg font-bold">PortFolio</span>
+                <span className="ml-4 text-lg font-bold">DevFolio</span>
               </div>
             </a>
           </div>
-          <div className="w-auto p-8">
-            {/* <ul className="-m-5 flex flex-wrap items-center">
-              <li className="p-5">
-                <a
-                  className="font-medium text-gray-600 hover:text-gray-700"
-                  href="#"
-                >
-                  Privacy Policy
-                </a>
-              </li>
-              <li className="p-5">
-                <a
-                  className="font-medium text-gray-600 hover:text-gray-700"
-                  href="#"
-                >
-                  Terms of Service
-                </a>
-              </li>
-              <li className="p-5">
-                <a
-                  className="font-medium text-gray-600 hover:text-gray-700"
-                  href="#"
-                >
-                  Return Policy
-                </a>
-              </li>
-              <li className="p-5">
-                <a
-                  className="font-medium text-gray-600 hover:text-gray-700"
-                  href="#"
-                >
-                  Contact Us
-                </a>
-              </li>
-            </ul> */}
-          </div>
-          <div className="w-auto p-8">
+          <div className="w-auto px-4 md:px-8 py-0 md:py-8">
             <div className="-m-1.5 flex flex-wrap">
               {buttonsData.map((item) => (
                 <div className="w-auto p-1.5" key={item.id}>
