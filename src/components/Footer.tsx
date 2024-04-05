b6b1cad83f34e78f3543b3ace6efa4d001c6808c
SauravBeginner <kundusaurav1996@gmail.com>
2024-04-06 02:41:52 +0530

    footer icons fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { buttonsData } from "@/app/data/dummyData";
+import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 
 function Footer() {
@@ -7,17 +8,17 @@ function Footer() {
       <div className="container relative z-10 mx-auto px-4">
         <div className="-my-8 flex flex-wrap items-center justify-center md:justify-between">
           <div className="w-auto px-4 md:px-8 py-0 md:py-8 ">
-            <a href="#">
+            <Link href="/">
               <div className="inline-flex items-center">
                 <span className="ml-4 text-lg font-bold">DevFolio</span>
               </div>
-            </a>
+            </Link>
           </div>
           <div className="w-auto px-4 md:px-8 py-0 md:py-8">
             <div className="-m-1.5 flex flex-wrap">
               {buttonsData.map((item) => (
                 <div className="w-auto p-1.5" key={item.id}>
-                  <a href="#">
+                  <a href={item.url} target="_blank" rel="noreferrer">
                     <div className="flex h-8 w-8 items-center justify-center rounded-full border border-gray-300 hover:border-gray-400">
                       <item.icon size={20} />
                     </div>
