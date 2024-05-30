e8d439168f54311674f329966f2fed3389496769
SauravBeginner <kundusaurav1996@gmail.com>
2024-05-31 02:00:11 +0530

    minor bug fixed 1

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,10 @@
-import { SignupComp } from "@/components/ContactForm";
+import { ContactForm } from "@/components/ContactForm";
 import React from "react";
 
 const Signup = () => {
   return (
     <>
-      <SignupComp />
+      <ContactForm />
     </>
   );
 };
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,21 +1,14 @@
 "use client";
 import React from "react";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
 import { cn } from "@/utils/cn";
-import {
-  IconBrandGithub,
-  IconBrandGoogle,
-  IconBrandOnlyfans,
-} from "@tabler/icons-react";
-import { TextArea } from "./ui/textArea";
+// import {
+//   IconBrandGithub,
+//   IconBrandGoogle,
+//   IconBrandOnlyfans,
+// } from "@tabler/icons-react";
 import { Email } from "./Email";
 
 export function ContactForm() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Form submitted");
-  };
   return (
     <div className="relative py-10 " id="contact">
       <div className="text-center my-10">
@@ -34,35 +27,6 @@ export function ContactForm() {
           Let's collaborate and discuss your project 🚀
         </p>
 
-        {/* <form className="my-8" onSubmit={handleSubmit}>
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-            <LabelInputContainer>
-              <Label htmlFor="firstname">First name</Label>
-              <Input id="firstname" placeholder="Tyler" type="text" />
-            </LabelInputContainer>
-            <LabelInputContainer>
-              <Label htmlFor="lastname">Last name</Label>
-              <Input id="lastname" placeholder="Durden" type="text" />
-            </LabelInputContainer>
-          </div>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="email">Email Address</Label>
-            <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
-          </LabelInputContainer>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="email">Message</Label>
-            <TextArea id="message" placeholder="Enter text..." />
-          </LabelInputContainer>
-
-          <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-          <button
-            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-            type="submit"
-          >
-            Submit
-            <BottomGradient />
-          </button>
-        </form> */}
         <Email />
       </div>
     </div>
